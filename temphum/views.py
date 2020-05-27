7793fdd006b6ac58d1b67118f7fdc9dc86b25603
Shiroke-013 <Shiroke4321@gmail.com>
2020-05-27 15:56:13 -0500

    Taller4

--- temphum/views.py
+++ temphum/views.py
@@ -14,12 +14,12 @@ def temphum(request):
         if value:
             # Crea el json para realizar la petición POST al Web Service
             args = {'type': 'humidity', 'value': value, 'latitude' : latitude, 'length':length, 'land':land}
-            response = requests.post('http://127.0.0.1:8000/temphum/', args)
+            response = requests.post('https://pi1-eafit-adchavesp.azurewebsites.net/temphum/', args)
             # Convierte la respuesta en JSON
             temphum_json = response.json()
 
     # Realiza una petición GET al Web Services
-    response = requests.get('http://127.0.0.1:8000/temphum/')
+    response = requests.get('https://pi1-eafit-adchavesp.azurewebsites.net/temphum/')
     # Convierte la respuesta en JSON
     temphum = response.json()
     # Rederiza la respuesta en el template measure
